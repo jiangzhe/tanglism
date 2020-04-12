a7be4a94ec152bc147ac01b70eba411ffc80a13b
Zhe Jiang <nju.jiangzhe@gmail.com>
2020-04-12 18:30:02 +0800

    fix issue on stroke processing

--- tanglism-morph/src/stroke.rs
+++ tanglism-morph/src/stroke.rs
@@ -20,8 +20,8 @@ struct StrokeShaper<'p, 't, T> {
     pts: &'p [Parting],
     tts: &'t T,
     sks: Vec<Stroke>,
-    tail: Vec<Parting>,
-    start: Option<Parting>,
+    // 表示未成笔的起点序列
+    pending: Vec<Parting>,
 }
 
 impl<'p, 't, T: TradingTimestamps> StrokeShaper<'p, 't, T> {
@@ -30,8 +30,7 @@ impl<'p, 't, T: TradingTimestamps> StrokeShaper<'p, 't, T> {
             pts,
             tts,
             sks: Vec::new(),
-            tail: Vec::new(),
-            start: None,
+            pending: Vec::new(),
         }
     }
 
@@ -41,97 +40,99 @@ impl<'p, 't, T: TradingTimestamps> StrokeShaper<'p, 't, T> {
         }
         let mut pts_iter = self.pts.iter();
         let first = pts_iter.next().cloned().unwrap();
-        self.start = Some(first.clone());
-        self.tail.push(first);
+        self.pending = vec![first];
         while let Some(pt) = pts_iter.next() {
             self.consume(pt.clone());
         }
         Ok(self.sks)
     }
 
-
+    // 依序消费每个分型
+    // 可以根据当前起点的分型，分为同类型，和不同类型
+    // 1. 顶底、底顶：可连成一笔
+    // 2. 顶顶、底底：无法连成一笔，单需要考虑如果底比前底低，
+    //    或者顶比前顶高，则需要修改前一笔的终点为该分型
     fn consume(&mut self, pt: Parting) {
-        self.tail.push(pt.clone());
-        if pt.top != self.start().top {
-            self.consume_diff_dir(pt);
-        } else {
-            self.consume_same_dir(pt);
-        }
-    }
-
-    fn consume_diff_dir(&mut self, pt: Parting) {
-        if self.is_start_neighbor(&pt) {
-            // 这里不做变化
-            // 可以保留的可能性是起点跳至pt点
-            return;
-        }
-        // 顶比底低
-        if (pt.top && pt.extremum_price <= self.start().extremum_price)
-            || (self.start().top && self.start().extremum_price <= pt.extremum_price)
-        {
-            return;
-        }
-        // 成笔
-        let new_sk = Stroke {
-            start_pt: self.start.take().unwrap(),
-            end_pt: pt.clone(),
-        };
-        self.start = Some(pt);
-        self.tail.clear();
-        self.sks.push(new_sk);
-    }
-
-    fn consume_same_dir(&mut self, pt: Parting) {
-        if self.is_start_neighbor(&pt) {
-            return;
-        }
-        // 顶比起点低，底比起点高
-        if (pt.top && pt.extremum_price < self.start().extremum_price)
-            || (!pt.top && pt.extremum_price > self.start().extremum_price)
-        {
+        // 存在前一笔时，比较当前的分型是否与前一笔的终点分型类型一致
+        // 如果一致，则比较高低，并根据情况修改笔或丢弃
+        if let Some(sk) = self.sks.last_mut() {
+            // 比较方向
+            if sk.end_pt.top == pt.top {
+                // 顶比前顶高，或者底比前底低，直接修改该笔
+                if (pt.top && pt.extremum_price > sk.end_pt.extremum_price) || (!pt.top && pt.extremum_price < sk.end_pt.extremum_price) {
+                    
+                    sk.end_pt = pt;
+                }
+            } else {
+                // 异向顶底间满足顶比底高，且有独立K线
+                if (pt.top && pt.extremum_price > sk.end_pt.extremum_price) || (!pt.top && pt.extremum_price < sk.end_pt.extremum_price) {
+                    if let Some(indep_ts) = self.tts.next_tick(sk.end_pt.end_ts) {
+                        if indep_ts < pt.start_ts {
+                            // 成笔
+                            let new_sk = Stroke{
+                                start_pt: sk.end_pt.clone(),
+                                end_pt: pt,
+                            };
+                            self.sks.push(new_sk);
+                        }
+                    }
+                }
+            }
+            // 不满足任一成笔条件则丢弃
             return;
         }
 
-        if let Some(last_sk) = self.sks.last_mut() {
-            // 有笔，需要修改笔终点
-            last_sk.end_pt = pt.clone();
-        }
-        self.start.replace(pt);
-        self.tail.clear();
-    }
-
-    fn is_start_neighbor(&self, pt: &Parting) -> bool {
-        if let Some(start) = self.start.as_ref() {
-            if let Some(indep_ts) = self.tts.next_tick(start.end_ts) {
-                if indep_ts < pt.start_ts {
-                    return false;
+        // 不存在前一笔，则需要和未成笔的潜在起点序列进行比较
+        let mut matches = Vec::new();
+        for p in &self.pending {
+            // 方向不同且顶比底高
+            if pt.top != p.top && ((pt.top && pt.extremum_price > p.extremum_price) || (!pt.top && pt.extremum_price < p.extremum_price)) {
+                // 比较独立K线
+                if let Some(indep_ts) = self.tts.next_tick(p.end_ts) {
+                    if indep_ts < pt.start_ts {
+                         // 成笔
+                        let new_sk = Stroke{
+                            start_pt: p.clone(),
+                            end_pt: pt.clone(),
+                        };
+                        matches.push(new_sk);
+                    }
                 }
             }
         }
-        true
-    }
-
-    #[inline]
-    fn start(&self) -> &Parting {
-        self.start.as_ref().unwrap()
+        // 与未成笔序列无法成笔时，加入未成笔序列
+        if matches.is_empty() {
+            self.pending.push(pt);
+            return;
+        }
+        // 在是否成笔的判断中，我们取差距更大的分型作为起点，
+        // 即如果有多个底可以和顶分型构成一笔，这里取较低的底。
+        // 反之亦然。
+        let mut r = matches.pop().unwrap();
+        while let Some(m) = matches.pop() {
+            if (&r.start_pt.extremum_price - &r.end_pt.extremum_price).abs() < (&m.start_pt.extremum_price - &m.end_pt.extremum_price).abs() {
+                r = m;
+            }
+        }
+        self.sks.push(r);
+        self.pending.clear();
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tanglism_utils::LOCAL_TS_1_MIN;
+    use tanglism_utils::{LOCAL_TS_1_MIN, LOCAL_TS_30_MIN, TradingTimestamps};
     use chrono::NaiveDateTime;
     use bigdecimal::BigDecimal;
     
     #[test]
     fn test_shaper_no_stroke() -> Result<()> {
         let sks = pts_to_sks_1_min(vec![
-            new_pt1("2020-02-01 10:00", 10.00, false),
-            new_pt1("2020-02-01 10:01", 10.10, true),
-            new_pt1("2020-02-01 10:03", 9.50, false),
-            new_pt1("2020-02-01 10:06", 9.80, true),
+            new_pt1("2020-01-07 10:00", 10.00, false),
+            new_pt1("2020-01-07 10:01", 10.10, true),
+            new_pt1("2020-01-07 10:03", 9.50, false),
+            new_pt1("2020-01-07 10:06", 9.80, true),
         ]);
         assert!(sks.is_empty());
         Ok(())
@@ -140,50 +141,86 @@ mod tests {
     #[test]
     fn test_shaper_one_stroke_simple() -> Result<()> {
         let sks = pts_to_sks_1_min(vec![
-            new_pt1("2020-02-01 10:00", 10.00, false),
-            new_pt1("2020-02-01 10:10", 10.40, true),
-            new_pt1("2020-02-01 10:13", 10.30, false),
+            new_pt1("2020-01-07 10:00", 10.00, false),
+            new_pt1("2020-01-07 10:10", 10.40, true),
+            new_pt1("2020-01-07 10:13", 10.30, false),
         ]);
         assert_eq!(1, sks.len());
         Ok(())
     }
 
+    // 一笔，起点移动，因为起点不是最低的底分型
     #[test]
     fn test_shaper_one_stroke_moving_start() -> Result<()> {
         let sks = pts_to_sks_1_min(vec![
-            new_pt1("2020-02-01 10:00", 10.00, false),
-            new_pt1("2020-02-01 10:02", 10.10, true),
-            new_pt1("2020-02-01 10:04", 9.90, false),
-            new_pt1("2020-02-01 10:10", 10.30, true),
+            new_pt1("2020-01-07 10:00", 10.00, false),
+            new_pt1("2020-01-07 10:02", 10.10, true),
+            new_pt1("2020-01-07 10:04", 9.90, false),
+            new_pt1("2020-01-07 10:10", 10.30, true),
         ]);
         assert_eq!(1, sks.len());
-        assert_eq!(new_ts("2020-02-01 10:04"), sks[0].start_pt.extremum_ts);
-        assert_eq!(new_ts("2020-02-01 10:10"), sks[0].end_pt.extremum_ts);
+        assert_eq!(new_ts("2020-01-07 10:04"), sks[0].start_pt.extremum_ts);
+        assert_eq!(new_ts("2020-01-07 10:10"), sks[0].end_pt.extremum_ts);
         Ok(())
     }
 
+    // 一笔，起点不一定，因为起点是最低的底分型
     #[test]
     fn test_shaper_one_stroke_non_moving_start() -> Result<()> {
         let sks = pts_to_sks_1_min(vec![
-            new_pt1("2020-02-01 10:00", 10.00, false),
-            new_pt1("2020-02-01 10:02", 10.10, true),
-            new_pt1("2020-02-01 10:04", 10.02, false),
-            new_pt1("2020-02-01 10:10", 10.30, true),
+            new_pt1("2020-01-07 10:00", 10.00, false),
+            new_pt1("2020-01-07 10:02", 10.10, true),
+            new_pt1("2020-01-07 10:04", 10.02, false),
+            new_pt1("2020-01-07 10:10", 10.30, true),
         ]);
         assert_eq!(1, sks.len());
-        assert_eq!(new_ts("2020-02-01 10:00"), sks[0].start_pt.extremum_ts);
-        assert_eq!(new_ts("2020-02-01 10:10"), sks[0].end_pt.extremum_ts);
+        assert_eq!(new_ts("2020-01-07 10:00"), sks[0].start_pt.extremum_ts);
+        assert_eq!(new_ts("2020-01-07 10:10"), sks[0].end_pt.extremum_ts);
         Ok(())
     }
 
+    // 简单的两笔
     #[test]
     fn test_shaper_two_strokes_simple() -> Result<()> {
         let sks = pts_to_sks_1_min(vec![
-            new_pt1("2020-02-01 10:00", 10.00, false),
-            new_pt1("2020-02-01 10:10", 10.10, true),
-            new_pt1("2020-02-01 10:20", 10.02, false),
+            new_pt1("2020-01-07 10:00", 10.00, false),
+            new_pt1("2020-01-07 10:10", 10.10, true),
+            new_pt1("2020-01-07 10:20", 10.02, false),
         ]);
         assert_eq!(2, sks.len());
+        Ok(())
+    }
+
+    // 一笔，分型跨天
+    #[test]
+    fn test_shaper_one_stroke_across_days() -> Result<()> {
+        let sks = pts_to_sks_30_min(vec![
+            new_pt30("2020-01-07 10:00", 10.00, true),
+            new_pt30("2020-01-08 10:00", 9.50, false),
+        ]);
+        assert_eq!(1, sks.len());
+        assert_eq!(new_ts("2020-01-07 10:00"), sks[0].start_pt.extremum_ts);
+        assert_eq!(new_ts("2020-01-08 10:00"), sks[0].end_pt.extremum_ts);
+        Ok(())
+    }
+
+    // 一笔，开盘跳空，3.11 ~ 3.13 贵州茅台
+    // 1. 后底低于前底
+    // 2. K线包含
+    #[test]
+    fn test_shaper_one_complex_stroke_across_days() -> Result<()> {
+        let sks = pts_to_sks_30_min(vec![
+            ts_pt30("2020-03-11 13:30", 1169.50, true, "2020-03-11 11:00", "2020-03-11 14:00"),
+            ts_pt30("2020-03-11 14:00", 1156.70, false, "2020-03-11 11:30", "2020-03-11 14:30"),
+            ts_pt30("2020-03-11 15:00", 1167.40, true, "2020-03-11 14:30", "2020-03-12 10:00"),
+            ts_pt30("2020-03-12 10:30", 1125.10, false, "2020-03-12 10:00", "2020-03-12 14:00"),
+            ts_pt30("2020-03-12 11:00", 1147.98, true, "2020-03-12 10:30", "2020-03-12 15:00"),
+            ts_pt30("2020-03-13 10:00", 1080.00, false, "2020-03-12 14:30", "2020-03-13 14:00"),
+            ts_pt30("2020-03-13 13:30", 1128.92, true, "2020-03-13 10:00", "2020-03-13 15:00"),
+        ]);
+        assert_eq!(1, sks.len());
+        assert_eq!(new_ts("2020-03-11 13:30"), sks[0].start_pt.extremum_ts);
+        assert_eq!(new_ts("2020-03-13 10:00"), sks[0].end_pt.extremum_ts);
         Ok(())
     }
 
@@ -192,26 +229,59 @@ mod tests {
     }
 
     fn new_pt1(ts: &str, price: f64, top: bool) -> Parting {
-        new_pt_fix_width(ts, 1, price, 3, top)
-    }
-
-    fn new_pt5(ts: &str, price: f64, top: bool) -> Parting {
-        new_pt_fix_width(ts, 5, price, 3, top)
-    }
-
-    fn new_pt30(ts: &str, price: f64, top: bool) -> Parting {
-        new_pt_fix_width(ts, 30, price, 3, top)
-    }
-
-    fn new_pt_fix_width(ts: &str, minutes: i64, extremum_price: f64, n: i32, top: bool) -> Parting {
         let extremum_ts = new_ts(ts);
-        let start_ts = extremum_ts - chrono::Duration::minutes(minutes);
-        let end_ts = extremum_ts + chrono::Duration::minutes(minutes);
+        let start_ts = LOCAL_TS_1_MIN.prev_tick(extremum_ts).unwrap();
+        let end_ts = LOCAL_TS_1_MIN.next_tick(extremum_ts).unwrap();
         Parting {
             start_ts,
             extremum_ts,
             end_ts,
-            extremum_price: BigDecimal::from(extremum_price),
+            extremum_price: BigDecimal::from(price),
+            n: 3,
+            top,
+        }
+    }
+
+    fn pts_to_sks_30_min(pts: Vec<Parting>) -> Vec<Stroke> {
+        pts_to_sks(&pts, &*LOCAL_TS_30_MIN).unwrap()
+    }
+
+    fn new_pt30(ts: &str, price: f64, top: bool) -> Parting {
+        let extremum_ts = new_ts(ts);
+        let start_ts = LOCAL_TS_30_MIN.prev_tick(extremum_ts).unwrap();
+        let end_ts = LOCAL_TS_30_MIN.next_tick(extremum_ts).unwrap();
+        Parting{
+            start_ts,
+            extremum_ts,
+            end_ts,
+            extremum_price: BigDecimal::from(price),
+            n: 3,
+            top,
+        }
+    }
+
+    fn ts_pt30(ts: &str, price: f64, top: bool, start_ts: &str, end_ts: &str) -> Parting {
+        let start_ts = new_ts(start_ts);
+        let end_ts = new_ts(end_ts);
+        let extremum_ts = new_ts(ts);
+        let mut start = start_ts;
+        let mut n = 1;
+        while let Some(next_ts) = LOCAL_TS_30_MIN.next_tick(start) {
+            n += 1;
+            if next_ts == end_ts {
+                break;
+            }
+            if n > 100 {
+                panic!("exceeds max n of single parting");
+            }
+            start = next_ts;
+        }
+
+        Parting{
+            start_ts,
+            extremum_ts,
+            end_ts,
+            extremum_price: BigDecimal::from(price),
             n,
             top,
         }
